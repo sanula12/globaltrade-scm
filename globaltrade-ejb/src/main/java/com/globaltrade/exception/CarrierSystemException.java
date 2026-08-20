a5d6f2177278e045755fc260481bc60f2152ce85
Sanula Kariyapperuma <sanula2021@gmail.com>
2026-08-20 16:46:18 +0530

    add CarrierSystemException to handle carrier-related errors

---
+++ globaltrade-ejb/src/main/java/com/globaltrade/exception/CarrierSystemException.java
@@ -0,0 +1,24 @@
+package com.globaltrade.exception;
+
+import javax.ejb.ApplicationException;
+
+
+public class CarrierSystemException extends RuntimeException {
+
+    private static final long serialVersionUID = 1L;
+    private final String carrierName;
+
+    public CarrierSystemException(String carrierName,String message) {
+
+        super("Carrier system error for: " + carrierName + ": " + message);
+        this.carrierName = carrierName;
+    }
+
+    public CarrierSystemException(String carrierName, String message, Throwable cause) {
+
+        super("Carrier system error for: " + carrierName + ": " + message, cause);
+    this.carrierName = carrierName;
+    }
+
+    public String getCarrierName() { return carrierName; }
+}
