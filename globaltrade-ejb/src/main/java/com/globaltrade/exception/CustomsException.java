9f3e12b47b51624e48c90cc68f25975655edc54f
Sanula Kariyapperuma <sanula2021@gmail.com>
2026-09-03 12:04:46 +0530

    fix: update import statement for ApplicationException to use jakarta package

--- globaltrade-ejb/src/main/java/com/globaltrade/exception/CustomsException.java
+++ globaltrade-ejb/src/main/java/com/globaltrade/exception/CustomsException.java
@@ -1,6 +1,6 @@
 package com.globaltrade.exception;
 
-import javax.ejb.ApplicationException;
+import jakarta.ejb.ApplicationException;
 
 @ApplicationException(rollback = true)
 public class CustomsException extends RuntimeException {
