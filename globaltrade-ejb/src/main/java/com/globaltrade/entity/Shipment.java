b95380f0a27257b62f37561680ccf162ce557b9e
Sanula Kariyapperuma <sanula2021@gmail.com>
2026-09-03 12:05:36 +0530

    fix: update pom.xml to use jakarta dependencies for Java EE

--- globaltrade-ejb/src/main/java/com/globaltrade/entity/Shipment.java
+++ globaltrade-ejb/src/main/java/com/globaltrade/entity/Shipment.java
@@ -1,7 +1,7 @@
 package com.globaltrade.entity;
 
 
-import javax.persistence.*;
+import jakarta.persistence.*;
 import java.io.Serializable;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
